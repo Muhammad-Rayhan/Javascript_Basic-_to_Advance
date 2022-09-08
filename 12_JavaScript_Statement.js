f7ed449431bd1c78a465459ec8f5408e65af2c14
Muhammad-Rayhan <rayhan.cse2k17@gmail.com>
2022-09-08 23:21:28 +0600

    JavaScript Practice 09-08-2022

---
+++ 12_JavaScript_Statement.js
@@ -0,0 +1,3 @@
+console.log("Hello Programmer"); //First Statement
+var str = "Hello"; //Second Statement
+var Number = 10; //Third Statement
---
+++ 13_Math_Object_JavaScript.js
@@ -0,0 +1,47 @@
+// Math Object Constants Value
+// console.log(Math.PI);
+// console.log(Math.E);
+// console.log(Math.SQRT2);
+// console.log(Math.SQRT1_2);
+// console.log(Math.LOG2E);
+// console.log(Math.LOG10E);
+// console.log(Math.LN2);
+// console.log(Math.LN10);
+// console.log(Number.parseInt(Math.LN10));
+
+// Math round()
+console.log(Math.round(4.6));
+console.log(Math.round(-4.6));
+
+//Math ceil()
+console.log(Math.ceil(4.2));
+console.log(Math.ceil(-4.2));
+
+//Math floor()
+console.log(Math.floor(4.2));
+console.log(Math.floor(-4.2));
+
+//Math trunc()
+console.log(Math.trunc(14.40));
+
+// Math sign()
+console.log(Math.sign(-1));
+
+// Math pow()
+$num = '3.6';
+console.log(Math.pow(8, Number.parseInt($num)));
+
+//Math sqrt()
+console.log(Math.sqrt(64));
+
+//Math abs()
+console.log(Math.abs(-10.456));
+
+// Math min()
+console.log(Math.min(100,40,5,-5,50,80));
+
+//Math max()
+console.log(Math.max(100,40,5,-5,50,80));
+
+//Math random();
+console.log(Math.random());
