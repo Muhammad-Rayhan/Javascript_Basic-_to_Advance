bc842f6d46ea1a735e971ab5df43bbf19409dce7
Muhammad-Rayhan <rayhan.cse2k17@gmail.com>
2022-09-18 10:28:12 +0600

    JavaScript Practice 09/18/2022

---
+++ 35_JavaScript_String_Comparison.js
@@ -0,0 +1,12 @@
+var str1 = "abc";
+var str2 = "ABC";
+
+if(str2 > str1)
+{
+    console.log(str2 + " is grethar than " + str1);
+} else {
+    console.log(str1 + " is grethar than " + str2);
+}
+
+// JS String Compailer Set Smaller z,y,x...c,b,a (Priority Value Heigh)
+// Than JS String Compailer Set Bigger Z,Y,X...C,B,A (Priority Value Low [Z,Y...B,A smaller than(<) z,y...b,a] )
