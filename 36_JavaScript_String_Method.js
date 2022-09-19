309bb0471e8d49323e86f235315d697400792350
Muhammad-Rayhan <rayhan.cse2k17@gmail.com>
2022-09-19 14:23:19 +0600

    JavaScript Practice 09/19/2022

---
+++ 36_JavaScript_String_Method.js
@@ -0,0 +1,17 @@
+var a = "I am";
+var b = "Rayhan";
+var c = a.concat(b); //Concate Two Variable.
+console.log(c);
+
+var str = "My name is Rayhan";
+console.log(str.substr(11, 3));
+
+console.log(str.charAt(11)); //Useing any index number to print Any charecter
+
+console.log(str.startsWith('My name')); // If str variable value start ('My name') than return output true else return output false
+console.log(str.endsWith('My name')); // If str variable value End ('Rayhan') than return output true else return output false
+
+console.log(str.toUpperCase()); // Make str variable value all upper case & output this value
+console.log(str.toLowerCase()); // Make str variable value all lower case & output this value
+
+console.log(str.split(' ')); // To use split make a sentense to array
